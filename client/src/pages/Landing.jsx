a487564618848856afe67b164daf71c2f435af41
alexqs96 <alexander.mamani.dev@gmail.com>
2023-10-11 18:07:40 -0300

    fixed titles and ui buttons

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 import HeaderLanding from "../components/Landing/HeaderLanding/HeaderLanding"
-import LandingBottom from "../components/Landing/LandingBottom/LandingBottom"
+import BottomLanding from "../components/Landing/BottomLanding/BottomLanding"
 
 const Landing = () => {
   return (
     <div className="flex flex-col w-full">
       <HeaderLanding/>
 
-      <LandingBottom />
+      <BottomLanding />
     </div>
   )
 }
