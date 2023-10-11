b567c23c4a1c6c002cf71c27da03a460eb12650e
vic-ferr <victorhfp2014@gmail.com>
2023-10-11 07:14:51 -0300

    fix style navar and footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,15 +1,14 @@
 const Footer = () => {
-    let mensaje = "Let's Play";
     return (
         <footer className="flex justify-evenly p-8 bg-gray-200 h-[160px]">
             <div>
-                <h2 className="text-xl font-bold">{mensaje}</h2>
+                <span className="font-bold text-[2rem]">Let’s play</span>
                 <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor.</p>
             </div>
             <div className="flex space-x-8">
                 <a href="/faq" className="text-base font-bold hover:underline">FAQ</a>
-                <a href="/about-us" className="text-base font-bold hover:underline">About Us</a>
-                <a href="/contact" className="text-base font-bold hover:underline">Contact</a>
+                <a href="/about-us" className="text-base font-bold hover:underline">Sobre nosotros</a>
+                <a href="/contact" className="text-base font-bold hover:underline">Contacto</a>
             </div>
         </footer>
     )
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,37 +1,37 @@
 import { Link } from "react-router-dom";
-import { CiGlobe } from "react-icons/ci";
-import { CiCircleQuestion } from "react-icons/ci"
+import { CiGlobe, CiCircleQuestion } from "react-icons/ci";
 const Navbar = () => {
-    let mensaje = "Let's Play";
 
     return (
-        <nav className="flex gap-2 w-full  h-[56px]  top-0 px-3 fixed  bg-gray-200 ">
+        <nav className="flex gap-2 w-full  h-[96px]  top-0 px-3 fixed  bg-gray-200 ">
             <div className="w-[20%] flex justify-center items-center h-full cursor-pointer" aria-describedby="Pagina de Inicio">
-                <Link to="/" aria-describedby="Pagina de Inicio" className="flex justify-center items-center hover:bg-slate-50 transition-colors h-full w-full">
-                    {mensaje}
+                <Link to="/" aria-describedby="Pagina de Inicio" className="flex justify-center items-center h-full w-full">
+                    <span className="font-bold text-[2rem]">Let’s play</span>
+
                 </Link>
             </div>
-            <ul className="flex justify-end items-center gap-3 w-[80%]  h-full ">
+            <ul className="flex justify-end items-center gap-4 w-[80%]  h-full mr-[100px] ">
 
                 <li className="w-[10%]  h-[100%] flex justify-center items-center">
-                    <Link className="flex justify-center w-full h-full">
-                        <CiCircleQuestion className="text-slate-600  hover:text-slate-900 transition-colors h-full w-[25%]" />
+                    <Link className="flex justify-center items-center w-full h-full text-slate-600">
+                        Ayuda
+                        <CiCircleQuestion className="text-slate-600  hover:text-slate-900 transition-colors h-[24px] w-[79px]" />
                     </Link>
                 </li>
                 <li className="w-[10%]  h-[100%] flex justify-center items-center">
-                    <Link className="flex justify-center w-full h-full">
-                        <CiGlobe className="text-slate-600  hover:text-slate-900 transition-colors h-full w-[25%]" />
+                    <Link className="flex justify-center items-center w-full h-full text-slate-600">
+                        Contactanos
+                        <CiGlobe className="text-slate-600  hover:text-slate-900 transition-colors h-[24px] w-[113px]" />
                     </Link>
                 </li>
-
-                <li className="w-[20%]  h-[60%]" >
-                    <Link to="/login" className="flex justify-center items-center bg-[#bebebe] text-white rounded-md hover:bg-slate-500 transition-colors cursor-pointer  h-full w-full">
-                        login
+                <li className="w-[15%]">
+                    <Link to="/register" className="flex justify-center items-center bg-[#bebebe] text-white rounded-xl hover:bg-slate-500 transition-colors cursor-pointer h-[40px] w-[158px]">
+                        registrarme
                     </Link>
                 </li>
-                <li className="w-[20%] h-[60%]">
-                    <Link to="/register" className="flex justify-center items-center bg-[#bebebe] text-white rounded-md hover:bg-slate-500 transition-colors cursor-pointer h-full w-full">
-                        registro
+                <li className="w-[15%] " >
+                    <Link to="/login" className="flex justify-center items-center bg-[#bebebe] text-white rounded-xl hover:bg-slate-500 transition-colors cursor-pointer  h-[40px] w-[158px]">
+                        iniciar sesion
                     </Link>
                 </li>
 
