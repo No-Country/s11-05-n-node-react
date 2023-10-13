3d97da2eb90a2b9bf64b2948d5139509eab2c38c
Dario Elguero <darioelguero@gmail.com>
2023-10-13 03:19:47 -0300

    Added "prettier" and executed "run format"

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,16 +1,16 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../navbar/Navbar'
-import Footer from '../footer/Footer'
+import { Outlet } from "react-router-dom";
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <main className='pt-14 pb-4'>
+      <main className="pt-14 pb-4">
         <Outlet />
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
