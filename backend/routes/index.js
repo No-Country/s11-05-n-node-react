6abfa3ce93698b7d2cac9939de2e888bec7d3e4e
Yein E <yeinneg@gmail.com>
2023-10-19 09:24:59 -0300

    add route team

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import routerUser from "./user.js";
-
+import routerTeam from "./teamRoute.js"
 const route = Router();
 
 route.use("/user", routerUser);
+route.use("/team", routerTeam);
 
 export default route;
