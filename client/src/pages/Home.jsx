1a0b637fc2ffeba8cb39733954d546f5eafb6dfb
vic-ferr <96598305+vic-ferr@users.noreply.github.com>
2023-10-19 22:26:04 -0300

    Revert "add routes home page"

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import SidebarHome from "../components/SidebarHome/SidebarHome";
 
 import HeaderHome from "../components/HeaderHome/HeaderHome";
-import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -11,9 +10,8 @@ const Home = () => {
         <section className="lg:pl-[25%] xl:pl-[2%] 2xl:pl-[13%]">
           <HeaderHome />
         </section>
-        <section className="flex bg-slate-300 mt-5 lg:pl-[28%] xl:pl-[24%] 2xl:pl-[16%] p-3 pt-20">
-
-          <Outlet />
+        <section className="flex bg-slate-300 mt-5 lg:pl-[28%] xl:pl-[24%] 2xl:pl-[16%] h-screen p-3 pt-20">
+          <h1>Bienvenido a Let’s play aqui va el contenido del home</h1>
         </section>
       </div>
     </>
