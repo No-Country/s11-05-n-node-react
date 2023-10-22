54fec8d366623d952efb6e00684b3cd340d235c6
Miguel guerrero <uannokaiserfrom@gmail.com>
2023-10-22 17:40:31 -0300

    fix formating code

--- backend/routes/teamRoute.js
+++ backend/routes/teamRoute.js
@@ -1,14 +1,37 @@
 import { Router } from "express";
-import { getTeam,getTeams,createTeam, getUserTeams } from "../controllers/team.js";
+import {
+  getTeam,
+  getTeams,
+  createTeam,
+  getUserTeams,
+  deleteTeam,
+} from "../controllers/team.js";
 import { ValidatorGeneral } from "../middleware/validatorGeneral.js";
 import { verifyJWT } from "../middleware/verifyJWT.js";
-import { teamValidator,getTeamValidator } from "../validators/teamValidator.js";
+import {
+  teamValidator,
+  getTeamValidator,
+  teamDeleteValidator,
+} from "../validators/teamValidator.js";
 
 const route = Router();
 
-route.get('/', verifyJWT,getTeams);
-route.get('/me',verifyJWT,getUserTeams);
-route.get('/:id',verifyJWT,getTeamValidator ,ValidatorGeneral ,getTeam);
-route.post("/createTeam", teamValidator,verifyJWT,  ValidatorGeneral, createTeam);
+route.get("/", verifyJWT, getTeams);
+route.get("/me", verifyJWT, getUserTeams);
+route.get("/:id", verifyJWT, getTeamValidator, ValidatorGeneral, getTeam);
+route.post(
+  "/createTeam",
+  teamValidator,
+  verifyJWT,
+  ValidatorGeneral,
+  createTeam
+);
+route.delete(
+  "/deletTeam/:id",
+  verifyJWT,
+  teamDeleteValidator,
+  ValidatorGeneral,
+  deleteTeam
+);
 
 export default route;
