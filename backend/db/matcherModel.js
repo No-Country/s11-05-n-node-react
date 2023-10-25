dff3180a9a491c9f1129421ad0db3480a85ab6b6
Miguel guerrero <uannokaiserfrom@gmail.com>
2023-10-25 18:53:19 -0300

    add model for matcher

---
+++ backend/db/matcherModel.js
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    nameTeams: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "team",
+        default: null,
+      },
+    ],
+    date: {
+      type: Date,
+      default: null,
+    },
+    description: {
+      type: String,
+      default: null,
+    },
+    required_players: {
+      type: Number,
+      default: null,
+    },
+
+    duration: {
+      type: String,
+      default: null,
+    },
+
+    category: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "category",
+        default: null,
+      },
+    ],
+
+    schedule: {
+      type: String,
+    },
+    location: {
+      type: String,
+      default: null,
+    },
+
+    members_in_team: {
+      type: Number,
+      default: null,
+    },
+    substitutes_in_team: {
+      type: Number,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("User", userSchema);
