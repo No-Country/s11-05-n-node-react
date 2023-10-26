729dbafaae80a81bb7e624a00f3d46016273a729
Dario Elguero <darioelguero@gmail.com>
2023-10-26 20:52:15 -0300

    removed code

--- client/src/components/Team/LoadTeams.jsx
+++ client/src/components/Team/LoadTeams.jsx
@@ -5,28 +5,11 @@ import TeamAvatar from "./TeamAvatar";
 
 function LoadTeams() {
   const teams = useSelector(state => state.team.myTeams);
-  console.log(teams);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(dispatch(listMyTeams));
   }, []);
-
-  // const loadTeams = () => {
-  //   const teams = [];
-
-  //   for (let index = 0; index < 3; index++) {
-  //     teams.push(
-  //       <div key={index} className="flex flex-col justify-center items-center">
-  //         <div className="w-16 h-16 rounded-full border-2 border-blue-500 bg-slate-300 "></div>
-
-  //         <p>Nombre equipo</p>
-  //       </div>
-  //     );
-  //   }
-
-  //   return teams;
-  // };
 
   return (
     <>
