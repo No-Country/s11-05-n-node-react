7c1f85d0994f1ed7ab5b2df39724f271774c7581
Dev Masiel <masiel31@msn.com>
2023-10-29 23:19:08 -0300

    Add delete local avatar

--- backend/util/cloudinaryAvatar.js
+++ backend/util/cloudinaryAvatar.js
@@ -1,16 +1,12 @@
 import {v2 as cloudinary} from 'cloudinary';
-//import cloudinary from 'cloudinary'
 import dotenv from 'dotenv'
 dotenv.config()
 
-//configuracion Cloudinary         
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-console.log(cloudinary.config());
 
 export default cloudinary;
 
