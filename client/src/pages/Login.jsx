809c979368edd1607cbd2d591e80730572f6f094
alexqs96 <alexander.mamani.dev@gmail.com>
2023-11-01 21:38:59 -0300

    fixing bugs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
-import { BsFacebook, BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../store/state/authSlice";
@@ -57,73 +56,71 @@ function Login() {
   };
 
   return (
-    <section className="flex justify-end w-full h-screen bg-[#1E1E1E] relative">
-      <div className="h-[50rem] w-[50rem] background-circle fixed"></div>
+    <section className="relative max-w-screen-xl w-[90%] mx-auto">
+      <span className="gradient"></span>
 
-      <div className="md:w-6/12  lg:w-6/12 xl:w-6/12 w-full  h-full flex justify-center items-center text-center z-50 ">
-        <div className="md:w-auto lg:w-auto xl:w-auto w-11/12 m-auto  bg-black p-[50px] rounded-2xl shadow-md">
-          <div className="text-center">
-            <h1 className="text-white md:text-5xl  text-2xl  font-bold">Let’s play</h1>
+      <div className="ml-auto sm:max-w-sm w-full sm:bg-[#151515] my-16 p-10 sm:p-12 rounded-2xl shadow-md">
+        <div className="text-center">
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-t from-slate-300 to-white text-5xl font-bold">
+            <span>{"Let's"}</span>{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-t from-green-700 via-[#B5FF16] to-[#B5FF16]">
+              Play
+            </span>
+          </h1>
 
-            <h2 className="mt-7 mb-7 text-sm text-white ">Iniciar sesión con correo electrónico</h2>
-          </div>
-
-          <form className="flex flex-col gap-6" onSubmit={e => handleSubmit(e)}>
-            <div className="flex flex-col relative">
-              <label className=" bg-black absolute top-0 left-2 text-gray-200 transform -translate-y-2 transition-transform origin-top text-sm px-2">
-                Correo electrónico
-              </label>
-              <input
-                type="email"
-                className="h-12 pl-2 border bg-black text-white rounded-sm border-white placeholder:bg-black placeholder:text-white"
-                value={username}
-                onChange={e => {
-                  setUsername(e.target.value);
-                }}
-              />
-            </div>
-
-            <div className="flex flex-col relative">
-              <label className="bg-black absolute top-0 left-2 text-gray-200 transform -translate-y-2 transition-transform origin-top text-sm px-2">
-                Contraseña
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                className="h-12 border bg-transparent rounded-sm text-white border-white pl-2"
-                value={password}
-                onChange={e => {
-                  setPassword(e.target.value);
-                }}
-              />
-
-              <span
-                className="absolute top-2 right-2 cursor-pointer"
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-              </span>
-            </div>
-            <Link to={"/recover"} className="text-links text-center text-xs mt-1 ">
-              ¿Olvidó su contraseña?
-            </Link>
-            {errores && <p>{errores} </p>}
-            <button className="py-4 px-6 bg-buttons text-white text-[16px] rounded-md">
-              Iniciar sesión{" "}
-            </button>
-          </form>
-
-          <div className="w-full flex text-3xl justify-center gap-5 mt-9">
-            <FcGoogle />
-            <BsFacebook color="white" />
-          </div>
-
-          <p className="text-center mt-9 text-sm text-white">
-            ¿No tiene una cuenta?{" "}
-            <Link to={"/register"} className="text-links">
-              Registrarse ahora
-            </Link>
-          </p>
+          <h2 className="mt-5 mb-7 text-transparent bg-clip-text bg-gradient-to-t from-slate-300 to-white">Ingresa con tu cuenta</h2>
         </div>
+
+        <form className="flex flex-col gap-6" onSubmit={e => handleSubmit(e)}>
+          <div className="flex flex-col relative">
+            <label className=" bg-black sm:bg-[#151515] rounded-md absolute top-0 left-2 text-gray-200 transform -translate-y-2 transition-transform origin-top text-sm px-2">
+              Correo electrónico
+            </label>
+            <input
+              type="email"
+              className="h-12 pl-2 border bg-black sm:bg-[#151515] text-white rounded-md border-[#B5FF16] placeholder:bg-black placeholder:text-white"
+              value={username}
+              onChange={e => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col relative">
+            <label className="bg-black sm:bg-[#151515] rounded-md absolute top-0 left-2 text-gray-200 transform -translate-y-2 transition-transform origin-top text-sm px-2">
+              Contraseña
+            </label>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="h-12 border bg-transparent rounded-md text-white border-[#B5FF16] pl-2 pr-10"
+              value={password}
+              onChange={e => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <span
+              className="absolute inset-y-0 right-3 h-fit my-auto cursor-pointer text-white"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <BsEyeFill size={20} /> : <BsEyeSlashFill size={20} />}
+            </span>
+          </div>
+          <Link to={"/recover"} className="text-links text-center text-xs mt-1 font-medium">
+            ¿Olvidó su contraseña?
+          </Link>
+          {errores && <p className="py-1.5 px-2.5 bg-red-100 text-red-500 w-fit rounded-md text-center text-sm mx-auto">{errores} </p>}
+          <button className="bg-gradient-to-b from-[#B5FF16] to-green-300 text-black font-semibold h-12 px-6 max-sm:w-full text-lg rounded-md pressable">
+            Iniciar sesión{" "}
+          </button>
+        </form>
+
+        <p className="text-center mt-9 text-sm text-white font-medium">
+          ¿No tiene una cuenta?{" "}
+          <Link to={"/register"} className="text-links">
+            Registrarse ahora
+          </Link>
+        </p>
       </div>
     </section>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,8 +3,7 @@ import { validateRegister } from "../utils/ValidateRegister";
 import { useNavigate, Link } from "react-router-dom";
 import { postRequest } from "../services/httpRequest";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
-
-import clearStateErrors from "../hooks/clearStateErrors";
+import toast, { Toaster } from "react-hot-toast";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -12,10 +11,10 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [repeatPassword, setrepeatPassword] = useState("");
   const [errores, setErrores] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+  const [showPassword, setShowPassword] = useState({
+    password: false,
+    confirm: false
+  });
 
   const navigate = useNavigate();
 
@@ -32,118 +31,138 @@ const Register = () => {
         );
 
         console.log(response);
-        navigate("/login");
-      } catch (error) {
-        setErrores("Error al intentar registrarse");
+        toast.success("Bienvenido")
 
-        clearStateErrors("", setErrores);
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      } catch (error) {
+        toast.error("Error al intentar registrarse")
+        setErrores("Error al intentar registrarse");
       }
     } else {
-      setErrores(validate.msg);
-
-      clearStateErrors("", setErrores);
+      setErrores(validate.msg)
     }
   };
 
   return (
     <>
-      <section className="w-full h-screen flex items-center justify-center bg-[#C5CBDE]">
-        <div className="w-screen h-screen flex flex-col items-center justify-center">
-          <div className="w-11/12 max-w-3xl mx-auto bg-[#F1F3FF] p-20 rounded-2xl shadow-md">
-            <h1 className="text-black md:text-5xl text-2xl font-bold mb-4">Regístrate</h1>
-            <p className="text-black text-sm font-medium mb-6">
-              Crea tu usuario en Let&apos;s play y comienza con la diversión
-            </p>
-            <form className="flex flex-col gap-6 " onSubmit={handleSubmit}>
-              <div className="flex flex-row gap-6 pt-5 py-3">
-                <div className="flex flex-1 flex-col relative">
-                  <label
-                    className="bg-[#F1F3FF] absolute top-0 left-2 text-gray-600 transform -translate-y-2 transition-transform origin-top text-sm focus:outline-none focus:ring"
-                    style={{ top: "-0.1rem" }}
-                  >
-                    &nbsp;Usuario&nbsp;
-                  </label>
-                  <input
-                    type="text"
-                    className="h-12 pl-2 border bg-transparent rounded-sm border-black w-full focus:outline-none focus:ring"
-                    onChange={e => {
-                      setUsername(e.target.value);
-                    }}
-                  />
-                </div>
-                <div className="flex flex-1 flex-col relative ">
-                  <label
-                    className="bg-[#F1F3FF] absolute top-0 left-2 text-gray-600 transform -translate-y-2 transition-transform origin-top text-sm"
-                    style={{ top: "-0.1rem" }}
-                  >
-                    &nbsp;Correo electrónico&nbsp;
-                  </label>
-                  <input
-                    type="email"
-                    className="h-12 pl-2 border bg-transparent rounded-sm border-black w-full focus:outline-none focus:ring"
-                    onChange={e => {
-                      setEmail(e.target.value);
-                    }}
-                  />
-                </div>
+      <Toaster />
+      <section className="w-full flex flex-col items-center justify-center">
+        <div className="max-w-3xl mx-auto sm:w-[90%] sm:bg-[#151515] p-8 sm:p-12 md:p-16 rounded-2xl shadow-md my-16 text-white">
+          <h1 className="md:text-5xl text-2xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-t from-slate-300 to-white pb-4">Regístrate</h1>
+          <p className="font-medium mb-6 text-transparent bg-clip-text bg-gradient-to-t from-slate-300 to-white">
+            Crea tu usuario en{" "}
+            <span>{"Let's"}</span>{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-t from-green-700 via-[#B5FF16] to-[#B5FF16]">
+              Play
+            </span>
+            {" "}y comienza con la diversión
+          </p>
+          <form className="flex flex-col gap-6 " onSubmit={handleSubmit}>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 pt-5 py-3">
+              <div className="flex flex-1 flex-col relative">
+                <label
+                  className="bg-black sm:bg-[#151515] rounded-md absolute top-0 left-3.5 px-1 transform -translate-y-2 transition-transform origin-top text-sm focus:outline-none focus:ring"
+                  style={{ top: "-0.1rem" }}
+                >
+                  &nbsp;Usuario&nbsp;
+                </label>
+                <input
+                  type="text"
+                  className="h-12 pl-2 border bg-transparent rounded-md border-[#B5FF16] w-full focus:outline-none focus:ring"
+                  onChange={e => {
+                    setUsername(e.target.value);
+                  }}
+                />
               </div>
-              <div className="flex flex-row gap-6">
-                <div className="flex flex-col relative w-[48%]">
-                  <label className="bg-[#F1F3FF] absolute top-0 left-2 text-gray-600 transform -translate-y-2 transition-transform origin-top text-sm">
-                    Contraseña
-                  </label>
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    className="h-12 border bg-transparent rounded-sm border-black pl-2"
-                    value={password}
-                    onChange={e => {
-                      setPassword(e.target.value);
-                    }}
-                  />
+              <div className="flex flex-1 flex-col relative ">
+                <label
+                  className="bg-black sm:bg-[#151515] rounded-md absolute top-0 left-3.5 px-1 transform -translate-y-2 transition-transform origin-top text-sm"
+                  style={{ top: "-0.1rem" }}
+                >
+                  &nbsp;Correo electrónico&nbsp;
+                </label>
+                <input
+                  type="email"
+                  className="h-12 pl-2 border bg-transparent rounded-md border-[#B5FF16] w-full focus:outline-none focus:ring"
+                  onChange={e => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <div className="flex flex-col relative">
+                <label className="bg-black sm:bg-[#151515] rounded-md absolute top-0 left-3.5 px-1 transform -translate-y-2 transition-transform origin-top text-sm">
+                  Contraseña
+                </label>
+                <input
+                  type={showPassword.password ? "text" : "password"}
+                  className="h-12 border bg-transparent rounded-md border-[#B5FF16] pl-2 pr-10"
+                  value={password}
+                  onChange={e => {
+                    setPassword(e.target.value);
+                  }}
+                />
 
-                  <span
-                    className="absolute top-2 right-2 cursor-pointer"
-                    onClick={togglePasswordVisibility}
-                  >
-                    {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-                  </span>
-                </div>
-                <div className="flex flex-1 flex-col relative">
+                <span
+                  className="absolute inset-y-0 right-3 h-fit my-auto cursor-pointer"
+                  onClick={() => setShowPassword({
+                    ...showPassword,
+                    password: !showPassword.password
+                  })}
+                >
+                  {showPassword.password ? <BsEyeFill size={20} /> : <BsEyeSlashFill size={20} />}
+                </span>
+              </div>
+              <div className="flex flex-1 flex-col relative">
+                <div className="flex flex-col relative">
                   <label
-                    className="bg-[#F1F3FF] absolute top-0 left-2 text-gray-600 transform -translate-y-2 transition-transform origin-top text-sm"
+                    className="bg-black sm:bg-[#151515] rounded-md absolute top-0 left-3.5 px-1 transform -translate-y-2 transition-transform origin-top text-sm"
                     style={{ top: "-0.1rem" }}
                   >
                     &nbsp;Confirmar contraseña&nbsp;
                   </label>
                   <input
-                    type={showPassword ? "text" : "password"}
-                    className="h-12 pl-2 border bg-transparent rounded-sm border-black w-full focus:outline-none focus:ring"
+                    type={showPassword.confirm ? "text" : "password"}
+                    className="h-12 pl-2 pr-10 border bg-transparent rounded-md border-[#B5FF16] w-full focus:outline-none focus:ring"
                     onChange={e => {
                       setrepeatPassword(e.target.value);
                     }}
                   />
+
+                  <span
+                    className="absolute inset-y-0 right-3 h-fit my-auto cursor-pointer"
+                    onClick={() => setShowPassword({
+                      ...showPassword,
+                      confirm: !showPassword.confirm
+                    })}
+                  >
+                    {showPassword.confirm ? <BsEyeFill size={20} /> : <BsEyeSlashFill size={20} />}
+                  </span>
                 </div>
               </div>
-              <div className="flex flex-col">
-                <label className="flex items-center text-xs text-gray-600 w-3/5 py-2">
-                  <input type="checkbox" className="mr-2" />
-                  Acepto los Términos y condiciones y autorizo el uso de mis datos de acuerdo a la
-                  Declaración de Privacidad.
-                </label>
-              </div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <label className="flex items-center text-xs py-2">
+                <input type="checkbox" className="mr-4" />
+                Acepto los Términos y condiciones y autorizo el uso de mis datos de acuerdo a la
+                Declaración de Privacidad.
+              </label>
+            </div>
 
-              {errores && <p>{errores} </p>}
-              <button className="bg-gray-500 text-white h-12 w-5/12 rounded-sm hover:bg-blue-600 self-start">
-                Crear cuenta
-              </button>
-            </form>
-            <p className="text-center mt-9 text-sm">
-              ¿Ya tiene una cuenta?{" "}
-              <Link to={"/login"} className="text-links">
-                Ingresar ahora
-              </Link>
-            </p>
-          </div>
+            {errores && <p className="py-1.5 px-2.5 bg-red-100 text-red-500 w-fit rounded-md">{errores}</p>}
+            <button onMouseOver={() => setErrores(null)} className="bg-gradient-to-b from-[#B5FF16] to-green-300 text-black font-semibold h-12 px-6 max-sm:w-full text-lg rounded-md self-start pressable">
+              Crear cuenta
+            </button>
+          </form>
+          <p className="text-center mt-9 text-sm font-medium">
+            ¿Ya tiene una cuenta?{" "}
+            <Link to={"/login"} className="text-links">
+              Ingresar ahora
+            </Link>
+          </p>
         </div>
       </section>
     </>
